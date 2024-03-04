ee488d21fc9716e6eb0c338a23460ee44271978f
gab <gabriel_al4k@yahoo.com>
2024-03-04 18:21:11 +0800

    add rainfall dto model

---
+++ RainfallDemo/Rainfall.Contracts/DTO/RainfallReadingDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainfall.Contracts.DTO
+{
+    internal class RainfallReadingDTO
+    {
+        public DateTime dateMeasured { get; set; }
+        public decimal amountMeasured { get; set; }
+    }
+}
